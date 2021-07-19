fdc103371e0736792dc908c62426c1792d9b3e86
Prajwalnazre <prajwal.nazre777@gmail.com>
2021-07-20 00:07:42 +0530

    Created api to fetch all posts

--- server/index.js
+++ server/index.js
@@ -7,11 +7,12 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
+
 
 app.use(express.json({limit: "30mb", extended: true}));
 app.use(express.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
+app.use('/posts', postRoutes);
 
 // URL copy pasted from the mongo db atlas account
 
