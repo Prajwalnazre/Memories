fbcaf7e0fbd431a3194965a89f0b89d1c42f5e17
Prajwalnazre <prajwal.nazre777@gmail.com>
2021-07-21 00:00:17 +0530

    Fixed cache errors and reinstalled node modules

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import useStyles from './styles';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+import ThumbUpAltRoundedIcon from '@material-ui/icons/ThumbUpAltRounded';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import moment from 'moment';
 
-const Post = () => {
+const Post = ({ post }) => {
     const classes = useStyles();
     return (
-        <h1>POST</h1>
+        <Card className="classes.card">
+            {/* <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
+            <div className={classes.overlay}>
+                <Typography variant="h6">{post.creator}</Typography>
+                <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+            </div>
+            <div className={classes.overlay2}>
+                <Button style={{color: 'white'}} size="small" onClick={() => {}}>
+                    <MoreHorizIcon fontSize="default" />
+                </Button>
+            </div> */}
+        </Card>
     );
 }
 
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Post from './Post/Post';
+import { Grid, CircularProgress } from '@material-ui/core';
 import useStyles from './styles';
 
 import { useSelector } from 'react-redux';
@@ -10,11 +11,19 @@ const Posts = () => {
 
     console.log(posts);
     return (
-        <>
-        <h1 className={classes.posts}>POSTS</h1>
-        <Post></Post>
-        <Post></Post>
-        </>
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                {
+                    posts.map(
+                        (post) => (
+                            <Grid key={post._id} item xs={12} sm={6}>
+                                <Post post={post} />
+                            </Grid>
+                        )
+                    )
+                }
+            </Grid>
+        )
     );
 }
 
