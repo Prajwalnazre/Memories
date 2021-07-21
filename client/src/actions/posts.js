18df7f4230433996763151e65e70ae9eba8c7c37
Prajwalnazre <prajwal.nazre777@gmail.com>
2021-07-21 20:51:11 +0530

    Delete bug fixed

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -37,7 +37,7 @@ export const updatePost = (id, post) => async(dispatch) => {
 export const deletePost = (id) => async(dispatch) => {
     try {
         await api.deletePost(id);
-        dispatch({type : 'DELETE', payload: id})        
+        dispatch({type : 'DELETE', payload: id})     
     } catch (error) {
         console.log(error);
     }
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -2,7 +2,7 @@
 export default (posts = [], action) => {
     switch (action.type) {
         case 'DELETE' :
-            return posts.filter((post) => post._id !== action.payload.id);
+            return posts.filter((post) => post._id !== action.payload);
         case 'UPDATE' :
             return posts.map((post) => post._id === action.payload._id ? action.payload : post);
         case 'FETCH_ALL' :
