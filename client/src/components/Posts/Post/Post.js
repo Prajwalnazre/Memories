7f4d9330329fa9c240576dce362a4c72461ddef1
Prajwalnazre <prajwal.nazre777@gmail.com>
2021-07-22 19:35:50 +0530

    Reduced message font and fixed hashtag bug

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -30,7 +30,7 @@ const Post = ({ post, setCurrentId }) => {
                     {post.title}
             </Typography>
             <CardContent>
-                <Typography variant="h5" gutterBottom>
+                <Typography variant="body2" color="textSecondary" component="p">
                     {post.message}
                 </Typography>
             </CardContent>
